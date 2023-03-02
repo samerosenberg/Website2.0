e11541c180ff9394473d4839654b8e4b4b93684a
Sam Rosenberg <serosenberg99@gmail.com>
2023-03-01 19:09:36 -0600

    Compartmentalize education folder

---
+++ src/education.tsx
@@ -0,0 +1,34 @@
+/**
+ * Component to print out Pitt text inside Pitt.txt
+ *
+ * @return {Pitt}  {JSX.Element}
+ */
+export function Pitt(): JSX.Element {
+    return (
+        <div>
+            <h2>
+                <strong>University of Pittsburgh</strong>
+            </h2>
+            <p>August 2017 - April 2021</p>
+            <br></br>
+            <p>
+                I graduated magna cum laude with a Bachelors Degree in computer
+                science and a minor in statistics.
+            </p>
+            <p>
+                I was able to take a multitude of classes ranging from the
+                fields of mathematics, history, psychology, philosophy and most
+                importantly computer science.
+            </p>
+            <p>
+                I am very thankful for my time and the wonderful friends I made
+                along the way while attending Pitt.
+            </p>
+            <br></br>
+            <p>
+                <strong>Hail to Pitt!</strong>
+            </p>
+            <br></br>
+        </div>
+    );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Helmet } from "react-helmet";
 import "./index.css";
 import { AboutMe, Contact } from "./about";
 import { Epic, TechBlue } from "./experience";
+import { Pitt } from "./education";
 import Typing from "react-typing-animation";
 
 var lines: JSX.Element[] = [];
@@ -221,41 +222,6 @@ function HelpText(): JSX.Element {
             <p>
                 Type <strong>help</strong> at any point to bring this screen
                 back up if you need a refresher!
-            </p>
-            <br></br>
-        </div>
-    );
-}
-
-/**
- * Component to print out Pitt text inside Pitt.txt
- *
- * @return {Pitt}  {JSX.Element}
- */
-function Pitt(): JSX.Element {
-    return (
-        <div>
-            <h2>
-                <strong>University of Pittsburgh</strong>
-            </h2>
-            <p>August 2017 - April 2021</p>
-            <br></br>
-            <p>
-                I graduated magna cum laude with a Bachelors Degree in computer
-                science and a minor in statistics.
-            </p>
-            <p>
-                I was able to take a multitude of classes ranging from the
-                fields of mathematics, history, psychology, philosophy and most
-                importantly computer science.
-            </p>
-            <p>
-                I am very thankful for my time and the wonderful friends I made
-                along the way while attending Pitt.
-            </p>
-            <br></br>
-            <p>
-                <strong>Hail to Pitt!</strong>
             </p>
             <br></br>
         </div>
